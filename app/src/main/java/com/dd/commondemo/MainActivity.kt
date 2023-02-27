e110f9df22ed0389eaea2f0d481ba40a6739621e
丁笛 <466820812@qq.com>
2023-02-27 21:55:42 +0800

    系统主题优化

--- app/src/main/java/com/dd/commondemo/MainActivity.kt
+++ app/src/main/java/com/dd/commondemo/MainActivity.kt
@@ -7,16 +7,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.dd.basiccompose.theme.CommonDemoTheme
+import com.dd.basiccompose.theme.AppThemeDefault
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            com.dd.basiccompose.theme.CommonDemoTheme {
+            AppThemeDefault{
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -37,7 +40,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    com.dd.basiccompose.theme.CommonDemoTheme {
+    AppThemeDefault {
         Greeting("Android")
     }
 }
