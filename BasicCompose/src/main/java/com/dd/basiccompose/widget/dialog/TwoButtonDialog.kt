558c8f5e696a5d79ccdbb48d30ad27c9883e7149
丁笛 <466820812@qq.com>
2023-03-02 13:42:29 +0800

    弹窗View

---
+++ BasicCompose/src/main/java/com/dd/basiccompose/widget/dialog/TwoButtonDialog.kt
@@ -0,0 +1,100 @@
+package com.dd.basiccompose.widget.dialog
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Collections
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TwoButtonDialog(
+    onDismissRequest: (() -> Unit) = {},
+    icon: @Composable (() -> Unit)? = null,
+    title: String = "",
+    titleStyle: TextStyle = MaterialTheme.typography.titleLarge,
+    confirm: String = "确定",
+    onConfirmClick: (() -> Unit) = {},
+    cancel: String = "取消",
+    onCancelClick: (() -> Unit) = {},
+    text: @Composable (() -> Unit) = { },
+) {
+    AlertDialog(
+        onDismissRequest = {
+            onDismissRequest.invoke()
+        },
+        icon = {
+            icon?.invoke()
+        },
+        title = {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(text = title, style = titleStyle)
+            }
+        },
+        text = {
+            text.invoke()
+        },
+        confirmButton = {
+            Button(
+                modifier = Modifier,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.background
+                ),
+                onClick = { onConfirmClick.invoke() }
+            ) {
+                Text(
+                    text = confirm,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+            }
+        },
+        dismissButton = {
+            Button(
+                modifier = Modifier,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.background
+                ),
+                onClick = { onCancelClick.invoke() }
+            ) {
+                Text(
+                    text = cancel,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onBackground,
+                )
+            }
+        }
+    )
+
+}
+
+
+@Preview
+@Composable
+fun TwoButtonDialogPreview() {
+    TwoButtonDialog(
+        icon = {
+            Icon(
+                imageVector = Icons.Default.Collections,
+                contentDescription = Icons.Default.Collections.name
+            )
+        },
+        title = "AlertDialog",
+        text = {
+            Text(
+                text = "AlertDialog 将根据可用空间来定位其按钮。" +
+                        "默认情况下，它将尝试将它们水平地放在彼此的旁边，" +
+                        "如果没有足够的空间，则退回到水平放置。还有另一个" +
+                        "版本的 Composable，它有一个按钮槽，可以提供自" +
+                        "定义的按钮布局"
+            )
+        },
+    )
+}
--- versionPlugin/src/main/java/com/dd/version/config.kt
+++ versionPlugin/src/main/java/com/dd/version/config.kt
@@ -39,7 +39,7 @@ object AndroidTest {
     const val junit = "junit:junit:4.13.2"
     const val ext = "androidx.test.ext:junit:1.1.3"
     const val espresso = "androidx.test.espresso:espresso-core:3.4.0"
-    const val compose = "androidx.compose.ui:ui-test-junit4:$compose_version"
+    const val compose = "androidx.compose.ui:ui-test-junit4:1.3.3"
 }
 
 object Kotlin {
