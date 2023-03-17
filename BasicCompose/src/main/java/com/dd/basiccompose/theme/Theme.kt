f8844972bb96be6cf2fa7ff6cd1865457f814470
丁笛 <dingd@meixing.com>
2023-03-17 10:28:28 +0800

    小说章节信息

--- BasicCompose/src/main/java/com/dd/basiccompose/theme/Theme.kt
+++ BasicCompose/src/main/java/com/dd/basiccompose/theme/Theme.kt
@@ -14,11 +14,10 @@ import com.dd.basiccompose.controller.LocalSystemUiController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80, secondary = PurpleGrey80, tertiary = Pink80
+
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40, secondary = PurpleGrey40, tertiary = Pink40
 
 )
 
