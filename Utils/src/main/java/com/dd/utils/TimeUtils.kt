225d6850270b69beb373e72a75054c622c4f12c7
丁笛 <dingd@meixing.com>
2023-03-22 09:55:38 +0800

    优化

--- Utils/src/main/java/com/dd/utils/TimeUtils.kt
+++ Utils/src/main/java/com/dd/utils/TimeUtils.kt
@@ -6,4 +6,40 @@ object TimeUtils {
         return System.currentTimeMillis()
     }
 
+    const val minute = 1000 * 60
+    const val hour = minute * 60
+    const val day = hour * 24
+
+    /**
+     * 返回距离现在系统的时间差
+     * @param time 需要处理的时间戳
+     * @param tip 不足一分钟返回的时间
+     * Day return x天
+     * Hour return x天x小时
+     * Minute return x天x小时x分钟
+     *
+     **/
+    fun getCurrentTimeSpan(time: Long,tip :String?): String {
+        var timeDifference = getCurTime() - time
+        var sb = StringBuilder()
+        tip?.let {
+            if (timeDifference < minute){
+                return tip
+            }
+        }
+        if (timeDifference / day > 0) {
+            sb = sb.append("${timeDifference / day}天")
+            timeDifference %= day
+        }
+        if (timeDifference / hour > 0) {
+            sb = sb.append("${timeDifference / hour}小时")
+            timeDifference %= hour
+        }
+        if (timeDifference / minute > 0) {
+            sb = sb.append("${timeDifference / minute}分钟")
+            timeDifference %= minute
+        }
+        return sb.toString()
+    }
+
 }
