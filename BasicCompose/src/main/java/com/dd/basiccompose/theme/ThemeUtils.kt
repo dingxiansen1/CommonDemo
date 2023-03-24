02dd926276049ed5bebac1e0f7050b3da53a6d52
丁笛 <466820812@qq.com>
2023-03-24 22:13:46 +0800

    样式列表

--- BasicCompose/src/main/java/com/dd/basiccompose/theme/ThemeUtils.kt
+++ BasicCompose/src/main/java/com/dd/basiccompose/theme/ThemeUtils.kt
@@ -1,6 +1,9 @@
 package com.dd.basiccompose.theme
 
+import android.content.res.Configuration
+import android.os.Build
 import com.dd.common.utils.DataStoreUtils
+import com.dd.utils.Utils
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 
@@ -17,6 +20,29 @@ object ThemeUtils {
             DataStoreUtils.getSyncData(IS_NIGHT_MODEL, false)
         )
     )
+
+    fun isNightTheme(): Boolean {
+        if (mAppTheme.value.mIsFollowTheSystem){
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                Utils.getApp().resources.configuration.isNightModeActive
+            } else {
+                Utils.getApp().resources.configuration.uiMode == Configuration.UI_MODE_NIGHT_YES
+            }
+        }
+        if (mAppTheme.value.mIsNightModel){
+            return true
+        }
+        return false
+    }
+
+    /**
+     * 设置当前主题相反的主题
+     * 注意:如果当前模式是跟随系统
+     * 那么改状态会丢失
+     **/
+    fun setUnCurTheme(){
+        setNightModel(!isNightTheme())
+    }
 
     fun setFollowTheSystem(value: Boolean) {
         mAppTheme.update {
