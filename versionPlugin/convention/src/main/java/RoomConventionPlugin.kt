dc6242dfc75f7e90f9fa4d1af156c2b64cd1d3c7
丁笛 <dingd@meixing.com>
2023-04-12 15:41:10 +0800

    电影搜索历史和数据库架构

--- versionPlugin/convention/src/main/java/RoomConventionPlugin.kt
+++ versionPlugin/convention/src/main/java/RoomConventionPlugin.kt
@@ -1,11 +1,20 @@
 import com.dd.version.Jetpack
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 
 class RoomConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         with(project) {
+
+            extensions.configure<KaptExtension> {
+                arguments {
+                    arg("room.schemaLocation", "$projectDir/schemas")
+                }
+            }
+
             dependencies {
                 add("implementation", Jetpack.room_runtime)
                 add("implementation", Jetpack.room)
@@ -14,5 +23,4 @@ class RoomConventionPlugin : Plugin<Project> {
             }
         }
     }
-
 }
